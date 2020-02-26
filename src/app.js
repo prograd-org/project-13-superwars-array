ed37b2056bd42b06efdc1bdaed0e099168bd4c2e
suyashthonte0 <suyashthonte0@gmail.com>
2020-02-26 09:15:35 +0530

    Project 13 day_3

--- src/app.js
+++ src/app.js
@@ -22,28 +22,52 @@ const PLAYERS = [
 ];
 
 // initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
+//let detailedPlayers = []
+//detailedPlayers.length = 16;
 
+const initPlayers = (players) => {
+    
     // Instead of forloop use Map method
     // Code here
+    let detailedPlayers = []
+    detailedPlayers.length = 16;
+    detailedPlayers = PLAYERS.map(myFunction)
+    {
+        var players = {
+            name : PLAYERS[i],
+            strength : getRandomStrength(),
+            image : src = "../project-13-superwars-array/src/images/super-"+(i+1)+".png",
+            type : "hero|villain"
+        }
+    }
 
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
+    let randomstr = Math.floor(Math.random()*100)+1;
+    return randomstr;
 }
 
 // Build player template
-const buildPlayers = (players, type) => {
+const buildPlayers = (players, type) => 
+{
     let fragment = '';
 
+    for (var i = 0; i < PLAYERS.length; i++) 
+    {
+        fragment = `
+        <div class="player">
+        <img src="${detailedPlayers[i].image}" alt = "superheroes">
+        <h3> ${detailedPlayers[i].name}</h3>
+        <h1>${detailedPlayers[i].strength}</h1>
+        </div>
+         `;
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    }
     return fragment;
 }
 

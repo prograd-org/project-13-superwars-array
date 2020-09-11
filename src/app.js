278865b2e682f2e22395f3ad4668c19c05597c28
vamshiduddu <44239287+vamshiduddu@users.noreply.github.com>
2020-09-11 15:48:37 +0530

    1372364

--- src/app.js
+++ src/app.js
@@ -23,27 +23,19 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayer = '';
-    detailedPlayers = players.map((player, i) => {
+    let detailedPlayers = players.map((player, i) => {
         return {
             name: player,
-            image: "images/super-" + (i + 1) + ".png",
             strength: getRandomStrength(),
-            type: Math.ceil(Math.random() * 50) % 2 == 0 ? "hero" : "villain"
+            image: "images/super-" + (i + 1) + ".png",
+            type: (i % 2 == 0) ? "hero" : "villain"
         }
     });
+
     // Instead of forloop use Map method
     // Code here
-    return detailedPlayer;
-}
 
-
-function getType(i) {
-    if (i % 2 == 0) {
-        return "hero";
-    } else {
-        return "villain";
-    }
+    return detailedPlayers;
 }
 
 // getting random strength
@@ -51,28 +43,21 @@ const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
-
+// Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = players.filter(player => player.type == type).map(player =>
+        `<div class="player">
+         <img src="${player.image}" alt="">
+         <div class="name">${player.name}</div>
+         <div class="strength">${player.strength}</div>
+         </div>`).join();
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-    fragment = players.filter(player => player.type == type)
-        .map(
-            player =>
-            `<div class="player">
-                <img src="${player.image}" alt="">
-                <div class="name">${player.name}</div>
-                <div class="strength">${player.strength}</div>
-            </div>`
-        ).join();
+
     return fragment;
 }
-// Instead of using for loop
-// Use chaining of Array methods - filter, map and join
-// Type your code here
-
 
 // Display players in HTML
 const viewPlayers = (players) => {

1d1d4efb9c1158ba3b70a0f7b5ffc3e441b34665
K-Gokila <bkgokila.faristha@gmail.com>
2021-10-26 23:30:16 +0530

    Gokila K 7330

--- src/app.js
+++ src/app.js
@@ -23,11 +23,19 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    //let detailedPlayers = '';
+    
 
     // Instead of forloop use Map method
     // Code here
-
+    let detailedPlayers= players.map((players, i) =>{
+        return{
+            name:players,
+            strength:getRandomStrength(),
+            image:`images/super-${i+1}.png`,
+            type:getRandomStrength()%2===0 ? "villan":"hero"
+        }
+})
     return detailedPlayers;
 }
 
@@ -38,12 +46,18 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    //let fragment = '';
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+   
+    let fragment = players.filter(players => players.type == type).map(players =>
+        `<div class="player">
+        <img src="${players.image}" alt="">
+        <div class="name">${players.name}</div>
+        <div class="strength">${players.strength}</div>
+        </div>`).join();
     return fragment;
 }
 

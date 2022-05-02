bfb5a32d96bf8fca9efb1d89e0125c1e49352821
Gurshaan7869 <Gurshaan7869@gmail.com>
2022-05-02 11:18:21 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -18,42 +18,54 @@ const PLAYERS = [
     "Thor",
     "Slayer",
     "Vader",
-    "Slingo"
+    "Slingo",
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
+    let detailedPlayers = [];
+    players.map((player, i) => {
+        let character = {};
+        character.name = player;
+        character.strength = getRandomStrength();
+        character.image = `images/super-${i + 1}.png`;
+        character.type = getRandomStrength() % 2 === 0 ? "hero" : "villain";
+        detailedPlayers.push(character);
+    });
 
     return detailedPlayers;
-}
+};
 
 // getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
-}
+};
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
-
+    let fragment = "";
+    fragment = players
+        .filter((player) => player.type.toLowerCase() === type.toLowerCase())
+        .map((player, i) => {
+            return `<div class="player">
+                <img src="${player.image}" alt="">
+                <div class="name">${player.name}</div>
+                <div class="strength">${player.strength}</div>
+            </div>`;
+        })
+        .join("");
     return fragment;
-}
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+    document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+    document.getElementById("villains").innerHTML = buildPlayers(
+        players,
+        "villain"
+    );
+};
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
-}
+};
